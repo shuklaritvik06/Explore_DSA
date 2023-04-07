d798a5f7d5f478c67767e1fb743ff0804c69ef3a
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-04-07 14:02:40 +0300

    added the destructor

--- reversed_linked_list/reverse.cpp
+++ reversed_linked_list/reverse.cpp
@@ -1,65 +1,75 @@
+#include <iostream>
+#include <string>
 
-#include<iostream>
-#include<string>
-
-struct Node{
-
-    char data;
-    Node* next;
-
+struct Node {
+  char data;
+  Node *next;
 };
-class List{
+
+class List {
 private:
-    Node *head;
+  Node *head;
+
 public:
-    explicit List(std::string);
-    void DisplayLinkedList();
-    void ReverseLinkedList();
-
+  explicit List(std::string);
+  ~List(); // destructor added
+  void DisplayLinkedList();
+  void ReverseLinkedList();
 };
-void List::DisplayLinkedList(){
-    Node*ptr= head;
-    while (ptr!=nullptr){
-        std::cout<<ptr->data;
-        ptr=ptr->next;
+
+List::List(std::string str) {
+  head = nullptr;
+  Node *tail = nullptr;
+  for (int i = 0; i < str.length(); i++) {
+    Node *new_node = new Node;
+    new_node->data = str[i];
+    new_node->next = nullptr;
+    if (head == nullptr) {
+      head = new_node;
+      tail = new_node;
+    } else {
+      tail->next = new_node;
+      tail = new_node;
     }
+  }
 }
-List::List(std::string str){
-    head = nullptr;
-    Node* tail = nullptr;
-    for (int i = 0; i < str.length(); i++){
-        Node* new_node= new Node;
-        new_node->data=str[i];
-        new_node->next=nullptr;
-        if (head==nullptr){
-            head=new_node;
-            tail=new_node;
-        }
-        else{
-            tail->next=new_node;
-            tail=new_node;
-        }
-    }
+
+// destructor implementation
+List::~List() {
+  Node *current = head;
+  while (current != nullptr) {
+    Node *next = current->next;
+    delete current;
+    current = next;
+  }
 }
+
+void List::DisplayLinkedList() {
+  Node *ptr = head;
+  while (ptr != nullptr) {
+    std::cout << ptr->data;
+    ptr = ptr->next;
+  }
+}
+
 void List::ReverseLinkedList() {
-    Node* prev = nullptr;
-    Node* current = head;
-    Node* next = nullptr;
-    while (current != nullptr) {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
-    }
-    head = prev;
-
-
+  Node *prev = nullptr;
+  Node *current = head;
+  Node *next = nullptr;
+  while (current != nullptr) {
+    next = current->next;
+    current->next = prev;
+    prev = current;
+    current = next;
+  }
+  head = prev;
 }
-int main(){
-    std::string str;
-    std::getline(std::cin, str);
-    List myList{str};
-    myList.reverse_linked_list();
-    myList.display_linked_list();
-    return 0;
+
+int main() {
+  std::string str;
+  std::getline(std::cin, str);
+  List myList{str};
+  myList.ReverseLinkedList();
+  myList.DisplayLinkedList();
+  return 0;
 }
