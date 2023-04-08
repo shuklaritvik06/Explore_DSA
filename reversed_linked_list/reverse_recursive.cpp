0970751b3f6a9c8ee10a060f3920d062b5bfd12a
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-04-08 18:32:42 +0300

    updated code based on google code style

--- reversed_linked_list/reverse_recursive.cpp
+++ reversed_linked_list/reverse_recursive.cpp
@@ -8,60 +8,60 @@ struct Node {
 
 class List {
 private:
-  Node *head;
+  Node *head_;
 
-  Node *AddNodesRecursively(std::string str, int index);
-  void DeleteNodesRecursively(Node *node);
+  Node *AddNodesRecursively_(std::string str, int index);
+  void DeleteNodesRecursively_(Node *node);
 
 public:
   explicit List(std::string str);
   ~List();
-  Node *ReverseLinkedList(Node *current, Node *prev);
-  void DisplayLinkedList(Node *node);
-  Node *get_head() { return head; }
+  Node *ReverseLinkedList_(Node *current, Node *prev);
+  void DisplayLinkedList_(Node *node);
+  Node *get_head_() { return head_; }
 };
 
 List::List(std::string str) {
-  head = nullptr;
-  AddNodesRecursively(str, 0);
+  head_ = nullptr;
+  AddNodesRecursively_(str, 0);
 }
 
-List::~List() { DeleteNodesRecursively(head); }
+List::~List() { DeleteNodesRecursively_(head_); }
 
-Node *List::AddNodesRecursively(std::string str, int index) {
+Node *List::AddNodesRecursively_(std::string str, int index) {
   if (index >= str.length()) {
     return nullptr;
   }
   Node *new_node = new Node;
   new_node->data = str[index];
-  new_node->next = AddNodesRecursively(str, index + 1);
-  head = new_node;
+  new_node->next = AddNodesRecursively_(str, index + 1);
+  head_ = new_node;
   return new_node;
 }
 
-void List::DisplayLinkedList(Node *head) {
-  if (head != nullptr) {
-    std::cout << head->data;
-    DisplayLinkedList(head->next);
+void List::DisplayLinkedList_(Node *head_) {
+  if (head_ != nullptr) {
+    std::cout << head_->data;
+    DisplayLinkedList_(head_->next);
   }
 }
 
-Node *List::ReverseLinkedList(Node *current, Node *prev) {
+Node *List::ReverseLinkedList_(Node *current, Node *prev) {
   if (current == nullptr) {
-    head = prev;
-    return head;
+    head_ = prev;
+    return head_;
   }
   Node *next = current->next;
   current->next = prev;
-  ReverseLinkedList(next, current);
-  return head;
+  ReverseLinkedList_(next, current);
+  return head_;
 }
 
-void List::DeleteNodesRecursively(Node *node) {
+void List::DeleteNodesRecursively_(Node *node) {
   if (node == nullptr) {
     return;
   }
-  DeleteNodesRecursively(node->next);
+  DeleteNodesRecursively_(node->next);
   delete node;
 }
 
@@ -69,7 +69,7 @@ int main() {
   std::string str;
   std::getline(std::cin, str);
   List myList{str};
-  myList.ReverseLinkedList(myList.get_head(), nullptr);
-  myList.DisplayLinkedList(myList.get_head());
+  myList.ReverseLinkedList_(myList.get_head_(), nullptr);
+  myList.DisplayLinkedList_(myList.get_head_());
   return 0;
 }
