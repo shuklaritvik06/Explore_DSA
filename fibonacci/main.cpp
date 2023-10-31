81a3254231b19262a616c85ef02065ea3a6bf61a
shukla rituparn <shukla.r@phystech.edu>
2023-10-31 16:28:32 +0300

    updated the code

--- fibonacci/main.cpp
+++ fibonacci/main.cpp
@@ -1,41 +1,36 @@
 #include <iostream>
+#include <cstdint>  // Include for uint64_t
 
-class Fibonacci{
+class Fibonacci {
 public:
-    unsigned long long fibonacci(unsigned long long  n)
-    {
-        unsigned long long  F[2][2] = { { 1, 1 }, { 1, 0 } };
-        if (n == 0)
+    uint64_t CalculateFibonacci(uint64_t n) {
+        uint64_t F[2][2] = {{1, 1}, {1, 0}};
+        if (n == 0) {
             return 0;
-        else if (n == 1 ||n==2){
+        } else if (n == 1 || n == 2) {
             return 1;
         }
-        power(F, n - 1);
-
+        Power(F, n - 1);
         return F[0][0];
     }
 
-
-    void power(unsigned long long F[2][2], unsigned long long n)
-    {
-        if (n == 0 || n == 1)
+    void Power(uint64_t F[2][2], uint64_t n) {
+        if (n == 0 || n == 1) {
             return;
-        unsigned long long  M[2][2] = { { 1, 1 }, { 1, 0 } };
-
-        power(F, n / 2);
-        multiply(F, F);
-
-        if (n % 2 != 0)
-            multiply(F, M);
+        }
+        uint64_t M[2][2] = {{1, 1}, {1, 0}};
+        Power(F, n / 2);
+        Multiply(F, F);
+        if (n % 2 != 0) {
+            Multiply(F, M);
+        }
     }
 
-    void multiply(unsigned long long F[2][2], unsigned long long M[2][2])
-    {
-        unsigned long long x = (F[0][0] * M[0][0] + F[0][1] * M[1][0])% 1000003;
-        unsigned long long y = (F[0][0] * M[0][1] + F[0][1] * M[1][1])% 1000003;
-        unsigned long long z = (F[1][0] * M[0][0] + F[1][1] * M[1][0])% 1000003;
-        unsigned long long w = (F[1][0] * M[0][1] + F[1][1] * M[1][1])% 1000003;
-
+    void Multiply(uint64_t F[2][2], uint64_t M[2][2]) {
+        uint64_t x = (F[0][0] * M[0][0] + F[0][1] * M[1][0]) % 1000003;
+        uint64_t y = (F[0][0] * M[0][1] + F[0][1] * M[1][1]) % 1000003;
+        uint64_t z = (F[1][0] * M[0][0] + F[1][1] * M[1][0]) % 1000003;
+        uint64_t w = (F[1][0] * M[0][1] + F[1][1] * M[1][1]) % 1000003;
         F[0][0] = x;
         F[0][1] = y;
         F[1][0] = z;
@@ -43,14 +38,10 @@ public:
     }
 };
 
-
-
-
-int main()
-{
-    unsigned long long n;
+int main() {
+    uint64_t n;
     std::cin >> n;
-    Fibonacci Fib;
-    std::cout<<Fib.fibonacci(n-1)<<std::endl;
+    Fibonacci fib;
+    std::cout << fib.CalculateFibonacci(n - 1) << std::endl;
     return 0;
 }
