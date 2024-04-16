379fd6f36b16a75593712e1e42c3022fb4d1e5f1
Rituparn Shukla <shukla.r@phystech.edu>
2024-04-16 10:20:06 +0300

    TASKB.NIM_GAME

--- Task_B.Nim_Game/main.cpp
+++ Task_B.Nim_Game/main.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 #include <vector>
 
-
-int sprague_grundy(int heap_size, const std::vector<int>& moves, std::vector<int> &grundy) {
-    if (grundy[heap_size]!= -1)
-        return grundy[heap_size];
+int sprague_grundy(int heap_size, const std::vector<int>& moves,
+                   std::vector<int>& grundy) {
+    if (grundy[heap_size] != -1) return grundy[heap_size];
 
     std::vector<bool> mex(heap_size + 1, false);
 
     for (int move : moves) {
         if (move <= heap_size) {
-            int next_grundy = sprague_grundy(heap_size-move, moves, grundy);
+            int next_grundy = sprague_grundy(heap_size - move, moves, grundy);
             mex[next_grundy] = true;
         }
     }
 
-    for (int index = 0; index <=heap_size+1; ++index) {
+    for (int index = 0; index <= heap_size + 1; ++index) {
         if (!mex[index]) {
             grundy[heap_size] = index;
             break;
@@ -24,8 +23,16 @@ int sprague_grundy(int heap_size, const std::vector<int>& moves, std::vector<int
 
     return grundy[heap_size];
 }
-
-bool isWinningPosition(const std::vector<int> &heaps, std::vector<int> &grundy) {
+std::vector<int> grundyArray(std::vector<int>& Moves) {
+    const int MAX = 10001;
+    std::vector<int> grundy(MAX, -1);
+    for (int index = 0; index < MAX; ++index) {
+        sprague_grundy(index, Moves, grundy);
+    }
+    return grundy;
+}
+bool isWinningPosition(const std::vector<int>& heaps,
+                       std::vector<int>& grundy) {
     int NUM = 0;
     for (auto element : heaps) {
         NUM ^= grundy[element];
@@ -35,33 +42,22 @@ bool isWinningPosition(const std::vector<int> &heaps, std::vector<int> &grundy) 
 
 int main() {
     int numberofmoves;
-    std::cin >>numberofmoves;
-
+    std::cin >> numberofmoves;
     std::vector<int> Moves(numberofmoves);
     for (int index = 0; index < numberofmoves; ++index) {
         std::cin >> Moves[index];
     }
+    std::vector<int> grundy = grundyArray(Moves);
     int numberOfPositions;
-    std::cin >>numberOfPositions;
-    const int MAX = 10001;                //In the task given 10000 max value, so precomputing
-    std::vector<int> grundy(MAX, -1);
-    for (int index = 0; index < MAX; ++index) {
-        sprague_grundy(index, Moves, grundy);
-    }
+    std::cin >> numberOfPositions;
     while (numberOfPositions--) {
         int numberofheapes;
         std::cin >> numberofheapes;
-
         std::vector<int> heaps(numberofheapes);
         for (int index = 0; index < numberofheapes; ++index) {
             std::cin >> heaps[index];
         }
-
-        if (isWinningPosition(heaps, grundy)) {
-            std::cout << "W";
-        } else {
-            std::cout << "L";
-        }
+        std::cout << (isWinningPosition(heaps, grundy) ? "W" : "L");
     }
     return 0;
 }
